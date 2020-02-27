419f43a1e2fac2941922d5083e654ff807f6fcaf
Jordan R <jaygerzero@gmail.com>
2020-02-26 20:59:49 -0500

    arrowfuncs

--- codeself.js
+++ codeself.js
@@ -11,3 +11,17 @@
  ************************************
  * // Arrow Function
  */
+
+//  function printMyName(){
+//      console.log(name);
+//  }
+
+//  printMyName('Max');
+
+//  const printMyName = (name) => {
+//      console.log(name);
+//  }
+
+//  const multiply = number => number * 2;
+//  console.log(multiply(2));
+ 
