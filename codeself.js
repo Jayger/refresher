e2f7c676842585dfc1cf4f5780d21b7a419ce400
Jordan R <jaygerzero@gmail.com>
2020-02-26 20:50:49 -0500

    let&const

---
+++ codeself.js
@@ -0,0 +1,13 @@
+// // let & const
+// let myName = 'Jordan';
+// console.log(myName);
+// // If I wanted to modify this variable I can
+
+// const yourName = 'Max';
+// console.log(yourName);
+// // If I wanted to modify this variable I cannot
+
+/**
+ ************************************
+ * // Arrow Function
+ */
