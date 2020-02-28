b13e651f607d166f1aff1d27db4fc58414925352
Jordan R <jaygerzero@gmail.com>
2020-02-27 20:18:20 -0500

    Spread and Rest Ops

--- codeself.js
+++ codeself.js
@@ -60,28 +60,55 @@
  * Properties and Methods (Classes)
  */
  
-class Human {
-          gender = 'male';
+// class Human {
+//           gender = 'male';
           
-          printGender() {
-              console.log(this.gender);
-          }
-      }
+//           printGender() {
+//               console.log(this.gender);
+//           }
+//       }
     
     
-      class Person extends Human {
-            name = 'Jordan';
-            gender = 'female';
+//       class Person extends Human {
+//             name = 'Jordan';
+//             gender = 'female';
           
-          printMyName = () => {
-              console.log(this.name);
-          }
-      }
+//           printMyName = () => {
+//               console.log(this.name);
+//           }
+//       }
     
-      const person = new Person();
-      person.printMyName();
-      person.printGender();
+//       const person = new Person();
+//       person.printMyName();
+//       person.printGender();
     
 // The purpose of this is to show the use of ES7 syntax writing 
 // Using Arrow functions and now using the super or constructor method calls
+
+/**
+ * *********************************
+ * Spread and Rest Operators
+ */
+
+// SPREAD
+//  const numbers = [1, 2, 3];
+//  const newNumbers = [...numbers, 4, 5, 6];
+//  console.log(newNumbers);
  
+// const person = {
+//     name: 'Max'
+// }
+
+// const newPerson = {
+//     ...person,
+//     age: 29
+// }
+
+// console.log()
+
+// REST
+// const filter = (...args) => {
+//     return args.filter(el => el === 1);
+// }
+
+// console.log(filter(1, 2, 3));
